3f8aeec82d99e66dd5b8bb99d5aa2db4ddf049b4
Stingray <mong3134@gmail.com>
2026-01-19 20:07:13 +0800

    easier level upload

---
+++ js/pages/AddLevel.js
@@ -0,0 +1,78 @@
+export default {
+    template: `
+        <main class="page-add-level">
+            <title>Add Level</title>
+            <h1>Add a New Level to the Demonlist</h1>
+            <p>Fill out the form below to generate a JSON file for a new level. Make sure to follow the instructions in the generated JSON section.</p>
+            <form @submit.prevent="generateJson">
+                <div class="form-group">
+                    <label for="name">Level Name</label>
+                    <input type="text" id="name" v-model="level.name" placeholder="e.g. My Awesome Level">
+                </div>
+                <div class="form-group">
+                    <label for="creators">Creators (comma-separated)</label>
+                    <input type="text" id="creators" v-model="creators" placeholder="e.g. John Smith, Jane Doe">
+                </div>
+                <div class="form-group">
+                    <label for="verifier">Verifier</label>
+                    <input type="text" id="verifier" v-model="level.verifier" placeholder="e.g. John Smith">
+                </div>
+                <div class="form-group">
+                    <label for="verification">Verification Video URL</label>
+                    <input type="url" id="verification" v-model="level.verification" placeholder="e.g. https://www.youtube.com/watch?v=...">
+                </div>
+                <div class="form-group">
+                    <label for="percentToQualify">Percent to Qualify</label>
+                    <input type="number" id="percentToQualify" v-model.number="level.percentToQualify" min="1" max="100" placeholder="e.g. 75">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="text" id="password" v-model="level.password" placeholder="e.g. 123456">
+                </div>
+                <button type="submit">Generate Level JSON</button>
+            </form>
+            <div v-if="generatedJson" class="generated-json">
+                <h2>Generated Level JSON</h2>
+                <p>Click the button below to download the generated JSON file. Make sure to save it in the /data folder with a .json extension (e.g., My Awesome Level.json). Then, add the level name to /data/_list.json.</p>
+                <button @click="downloadJson">Download Level JSON</button>
+            </div>
+        </main>
+    `,
+    data() {
+        return {
+            level: {
+                id: Math.floor(Math.random() * 100000000),
+                name: '',
+                creators: [],
+                verifier: '',
+                verification: '',
+                percentToQualify: '',
+                password: '',
+                records: [],
+            },
+            creators: '',
+            generatedJson: null,
+        };
+    },
+    methods: {
+        generateJson() {
+            this.level.creators = this.creators.split(',').map(c => c.trim()).filter(c => c);
+            if (this.level.password === '') {
+                this.level.password = 'Free to Copy';
+            }
+            if (this.level.percentToQualify === '') {
+                this.level.percentToQualify = 100;
+            }
+            this.generatedJson = JSON.stringify(this.level, null, 4);
+        },
+        downloadJson() {
+            const blob = new Blob([this.generatedJson], { type: 'application/json' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${this.level.name}.json`;
+            a.click();
+            URL.revokeObjectURL(url);
+        },
+    },
+};
--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import AddLevel from './pages/AddLevel.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/add-level', component: AddLevel },
 ];
